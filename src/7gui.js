6cca1dd0066876505311412eb80b989eb6dba0f4
Dave Clayton <davedx@gmail.com>
2015-09-14 13:25:36 +0200

    Formatting

--- src/7gui.js
+++ src/7gui.js
@@ -1,13 +1,12 @@
 import {numberField, button, clicked} from "./framework";
 
-//FIXME: hide bootstrapping
 //TODO: lifecycle (mount/unmount to prevent memory leaks)
 function run() {
 	numberField("counter", 0);
 	button("count", "Click me");
 
 	clicked("count")
-    .set("counter", (input) => input + 1);
+		.set("counter", (input) => input + 1);
 }
 
 window.run = run;
